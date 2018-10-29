01fc2426de170ac725d4992ae2bbb5dd62709859
Nick McNeany <nmcneany@gmail.com>
2018-10-29 05:45:46 -0500

    initial devise setup

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,6 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+   
+  devise_for :accounts, path_names: {sign_in: "login", sign_out: "logout", sign_up: "register"}
+  
+  root to: "pages#home"
 end
