3ee9fe309a58c5f2beb44cdb1e74bf33e371b400
Nick McNeany <nmcneany@gmail.com>
2018-11-13 18:21:10 -0600

    add flash helper

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -24,4 +24,13 @@ module ApplicationHelper
          "#{@baby.name}'s Dashboard"      
       end
    end
+
+   def flash_class(key)
+      case key
+      when "success" then "alert alert-success"
+      when "error" then "alert alert-danger"
+      when "alert" then "alert alert-danger"
+      when "notice" then "alert alert-success"
+      end
+   end
 end
