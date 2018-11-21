df681e8b81e8377ddfb9f1d479283620833f6e3e
Nick McNeany <nmcneany@gmail.com>
2018-11-21 16:25:15 -0600

    add redirect after settings edit

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -5,4 +5,8 @@ class RegistrationsController < Devise::RegistrationsController
   def after_sign_up_path_for(resource)
    babies_start_path
   end
+
+  def after_update_path_for(resource)
+   edit_account_registration_path
+  end
 end
