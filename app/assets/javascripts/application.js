2307534f6f4b2b41b963a02d51d8d2780874bbc4
Nick McNeany <nmcneany@gmail.com>
2019-01-06 09:24:11 -0600

    mobile navs complete

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -17,6 +17,7 @@
 //= require moment
 //= require tempusdominus-bootstrap-4.js
 //= require activestorage
+//= require js-offcanvas.pkgd
 //= require turbolinks
 //= require_tree .
 
@@ -89,4 +90,51 @@ document.addEventListener("turbolinks:load", function() {
          }
       }) 
    }
+
+   (function($){
+      $.fn.appendAround = function(){
+         return this.each(function(){
+
+            var $self = $( this ),
+                  att = "data-set",
+                  $parent = $self.parent(),
+                  parent = $parent[ 0 ],
+                  attval = $parent.attr( att ),
+                  $set = $( "["+ att +"='" + attval + "']" );
+
+            function isHidden( elem ){
+               return $(elem).css( "display" ) === "none";
+            }
+
+            function appendToVisibleContainer(){
+               if( isHidden( parent ) ){
+                  var found = 0;
+                  $set.each(function(){
+                     if( !isHidden( this ) && !found ){
+                        $self.appendTo( this );
+                        found++;
+                        parent = this;
+                     }
+                  });
+               }
+            }
+
+            appendToVisibleContainer();
+
+            $(window).bind( "resize", appendToVisibleContainer );
+
+         });
+      };
+   }( jQuery ));
+   $( function(){
+      $( document ).trigger( "enhance" );
+      $( '#left' ).offcanvas( {
+         modifiers: "left,overlay",
+         triggerButton: '.js-offcanvas-toggler',
+         onInit :  function() {
+            $(this).removeClass('is-hidden');
+         }
+      } );
+      $( ".js-append-around" ).appendAround();
+   });
 });
