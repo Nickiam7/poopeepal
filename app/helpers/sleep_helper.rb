e53080a68ca0819a353821fcf453f44ab864d9ef
Nick McNeany <nmcneany@gmail.com>
2019-08-12 20:05:12 -0500

    add sleep helper

--- app/helpers/sleep_helper.rb
+++ app/helpers/sleep_helper.rb
@@ -1,2 +1,11 @@
 module SleepHelper
+   def sleep_converter(sleep)
+      duration = (sleep.end_time.to_time.to_i - sleep.start_time.to_time.to_i) / 60
+      if duration < 60
+         duration.to_s + " minutes"
+      else
+         hour_duration = duration / 60
+         pluralize(hour_duration, 'hour')
+      end
+   end
 end
