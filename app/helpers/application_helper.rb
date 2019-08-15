1938d21c1e030e5e61ecc5d39f1701bf0e29be8b
Nick McNeany <nmcneany@gmail.com>
2019-08-14 20:05:50 -0500

    move dashboard header into its own helper.

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -15,24 +15,6 @@ module ApplicationHelper
       @devise_mapping ||= Devise.mappings[:account]
    end
 
-   def dashboard_heading
-      if current_page?(root_path) || current_page?("/dashboard")
-         "Your Account Dashboard"
-      elsif current_page?(new_baby_path)    
-         "Create Your Baby's New Dashboard"
-      elsif current_page?(baby_path(@baby))
-         "#{@baby.name}'s Dashboard"
-      elsif current_page?(edit_baby_path(@baby))
-         "Edit #{@baby.name}'s Dashboard"
-      elsif current_page?(baby_entry_path(@baby, @entry))
-         "#{@entry.created_at.strftime("%A, %b %d, %Y")}"
-      elsif current_page?(new_baby_entry_feeding_path(@baby, @entry))
-         "Tracking New Feeding"
-      else
-         "PooPeePal"
-      end
-   end
-
    def flash_class(key)
       case key
       when "success" then "alert alert-success"
