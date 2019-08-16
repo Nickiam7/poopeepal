91ff2beababc31c1a6f5d0ab06c05221bc1687d0
Nick McNeany <nmcneany@gmail.com>
2019-08-15 21:15:31 -0500

    add total sleep time to baby dash and entry show

--- app/models/entry.rb
+++ app/models/entry.rb
@@ -10,17 +10,28 @@ class Entry < ApplicationRecord
       self.feedings.sum(&:amount)
    end
 
+   def sleep_time_ui(hrs, mins)
+      if mins == 0
+         "<h3>#{hrs}</h3><p>hrs</p>"
+      else
+         "<h3>#{hrs}</h3><p>" + "Hour".pluralize(hrs) + "</p> <h3>#{mins}</h3><p>" + "minute".pluralize(mins) + "</p>"
+      end
+   end
    def total_sleep_time
       sleep_time = []
       self.sleeps.each do |sleep|
          sleep_time << (sleep.end_time.to_time.to_i - sleep.start_time.to_time.to_i) / 60
       end
-      hrs = sleep_time.inject(:+) / 60
-      mins = sleep_time.inject(:+) % 60
-      if mins == 0
-         "<h3>#{hrs}</h3><p>hrs</p>"
-      else
-         "<h3>#{hrs}</h3><p>hrs</p> <h3>#{mins}</h3><p>mins</p>"
+      hrs = sleep_time.inject(:+) / 60 if sleep_time.length >= 1
+      mins = sleep_time.inject(:+) % 60 if sleep_time.length >= 1
+      sleep_time_ui(hrs, mins)
+   end
+
+   def total_sleep_time_entry
+      sleep_time = []
+      self.sleeps.each do |sleep|
+         sleep_time << (sleep.end_time.to_time.to_i - sleep.start_time.to_time.to_i) / 60
       end
+      hrs = sleep_time.inject(:+) / 60 if sleep_time.length >= 1
    end
 end
